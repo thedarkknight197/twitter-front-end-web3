c2a58288f319743c00acc9b4de3de68129b760eb
Michele Vitiello Bonaventura <michele.vitiellobonaventura@gmail.com>
2022-02-17 19:54:38 +0100

    Add Metamask button login

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,27 @@ export default function App() {
     checkIfWalletIsConnected();
   }, [])
 
+  /**
+  * Implement your connectWallet method here
+  */
+   const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   const wave = () => {
    
@@ -57,6 +78,15 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
