024c9ba3e4e2fbb44c255e3007d3751dd89dd519
Michele Vitiello Bonaventura <michele.vitiellobonaventura@gmail.com>
2022-02-18 12:19:48 +0100

    Add Wave functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from "../utils/WavePortal.json";
+
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0x97cEa707f9B7A2fAC674433Ca813E0b027eF484c";
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -33,10 +37,6 @@ export default function App() {
     }
   }
 
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, [])
-
   /**
   * Implement your connectWallet method here
   */
@@ -58,9 +58,40 @@ export default function App() {
     }
   }
 
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
-  const wave = () => {
-   
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
   
   return (
